7322955beb7ac5ad8452621a7750a431fe87004a
amaanirfan <amaanirf@gmail.com>
2020-09-02 13:36:17 -0700

    finished "rough draft" of the encoder, must test and compile

--- src/LZWEncoder.java
+++ src/LZWEncoder.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.*;
 
 public class LZWEncoder {
 	private ArrayList<String> key; //index of a string is the code associated with it, ex: A is the 0th element, B is the 1st element, ...
@@ -15,7 +20,24 @@ public class LZWEncoder {
 	}
 	
 	//main method that does the encoding from an input file to an output file
-	public void encode(String inputFile, String outputFile) {
-		PrintWriter pWriter = new PrintWriter(new FileReader (outputFile));
+	public void encode(String inputFile, String outputFile) throws IOException {
+		PrintWriter pWriter = new PrintWriter(new File(outputFile));
+		BufferedReader bReader = new BufferedReader(new FileReader(inputFile));
+		
+		String tempKeyStart = "" + (char)(bReader.read()); //this is the piece of the string being read that is already in our key
+		String tempKeyLast = "" + (char)(bReader.read()); //this is the last character of the string being read
+		
+		while(bReader.ready()) { //loops through the inputFile
+			if(!key.contains(tempKeyStart + tempKeyLast)) { //checks if the read in string is already in the key
+				pWriter.println(key.indexOf(tempKeyStart));
+				key.add(tempKeyStart+tempKeyLast);
+				tempKeyStart = tempKeyLast; //must do this for the stored string to reset
+			}
+			else {
+				tempKeyStart = tempKeyStart + tempKeyLast; //if it is already in the key, we just grow the start until we find a series of characters not in the key
+			}
+			
+			tempKeyLast = "" + (char)(bReader.read()); //adds to the string we are reading in
+		}
 	}
 }
