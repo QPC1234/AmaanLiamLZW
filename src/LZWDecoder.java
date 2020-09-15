bab322206efbb220f55a343d7134640153588a0d
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-15 10:13:23 -0700

    removed bad code from decoder
    all the code i wrote was wrong

--- src/LZWDecoder.java
+++ src/LZWDecoder.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LZWDecoder {
 
+	int maxSize = 4096;
+	
+	public LZWDecoder(){
+
+	}
+	
+	//main method that does the encoding from an input file to an output file
+	public void decode(String inputFile, String outputFile) throws IOException {
+		
+	}
 }
