f232c88951fe8024afc861584c3c9220d52acbbe
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-15 10:31:27 -0700

    added hashmap and chars + vals to decoder
    hashmap instantiated
    for loop

--- src/LZWDecoder.java
+++ src/LZWDecoder.java
@@ -1,16 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LZWDecoder {
-
+	private HashMap<Integer, String> key;
+	
 	int maxSize = 4096;
 	
 	public LZWDecoder(){
-
+		key = new HashMap<Integer, String>();
+		//add all the Ascii characters to the key to start with
+		for (int i = 0; i < 256; i++) {
+			key.put(i, "" + (char) i);
+		}
 	}
 	
-	//main method that does the encoding from an input file to an output file
+	//main method that does the decoding from an input file to an output file
 	public void decode(String inputFile, String outputFile) throws IOException {
+		//PrintWriter pWriter = new PrintWriter(outputFile);
+		//BufferedReader bReader = new BufferedReader(new FileReader(inputFile));
+		
 		
 	}
 }
