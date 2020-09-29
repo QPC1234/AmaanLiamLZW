dc1938b064a30fbc26bb63a064e1a823dbff6ed5
QPC1234 <quinncallaghan@gmail.com>
2020-09-29 09:26:01 -0700

    tester update

--- src/TesterLZW.java
+++ src/TesterLZW.java
@@ -5,6 +5,8 @@ public class TesterLZW {
 		LZWEncoder encoder = new LZWEncoder ();
 		LZWDecoder decoder = new LZWDecoder();
 		encoder.encode ("big.txt", "output.txt");
+		System.out.println("Done");
 		decoder.decode ("output.txt","decoded.txt");
+		System.out.println("Done");
 	}
 }
